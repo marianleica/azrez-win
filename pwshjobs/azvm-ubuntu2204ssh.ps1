f7b3996d454f31a47e877caf50c4dff5c1c695e0
marianleica <maleica@microsoft.com>
2024-10-16 12:12:41 +0300

    Adjusted code to match requirements

--- pwshjobs/azvm-ubuntu2204ssh.ps1
+++ pwshjobs/azvm-ubuntu2204ssh.ps1
@@ -6,43 +6,47 @@ Start-Sleep -Seconds 1
 # Setting variables
 $suffix=$(Get-Random -Minimum 10000 -Maximum 99999)
 #suffix=$((10000 + RANDOM % 99999))
-$rg="azrez"
+$RG="azrez"
 $location="uksouth"
-$vmName="azvm-windows11-${suffix}"
+$VM="azvm-windows11-${suffix}"
 $image="Ubuntu2204"
 $publicIp="win11IP-${suffix}"
 
 # Generating a random string to use as password
-$UserName = "admin"
+$UserName = "azrez"
 $randompass = -join ((48..57) + (65..90) + (97..122) | Get-Random -Count 30 | ForEach-Object {[char]$_})
 #Read more: https://www.sharepointdiary.com/2020/04/powershell-generate-random-password.html#ixzz8XiwccFos
 $Password = ConvertTo-SecureString $randompass -AsPlainText -Force
 $psCred = New-Object System.Management.Automation.PSCredential($UserName, $Password)
 
-Write-Output $"Creating virtual machine ${vmName} in resource group ${rg} in location ${location}"
+Write-Output $"Creating virtual machine ${vmName} in resource group ${RG} in location ${location}"
 Start-Sleep -Seconds 1
 Write-Output ""
+
 Write-Output "The Resource Group:"
 # Create RG
-New-AzResourceGroup -Name $rg -Location $location
+az group create -n $RG -l $location
 Start-Sleep -Seconds 1
 Write-Output ""
 Write-Output "The virtual machine {$vmName}:"
 
-# Create Windows 11
-New-AzVm -ResourceGroupName $rg -Name $vmName -Location $location -Image $image -VirtualNetworkName "myVnet-${suffix}" -SubnetName "vmsubnet" -SecurityGroupName "vmNSG" -PublicIpAddressName $publicIp -OpenPorts 80,22 -GenerateSshKey
+# Create Ubuntu VM
+# New-AzVm -ResourceGroupName $RG -Name $vmName -Location $location -Image $image -VirtualNetworkName "myVnet-${suffix}" -SubnetName "vmsubnet" -SecurityGroupName "vmNSG" -PublicIpAddressName $publicIp -OpenPorts 80,22 -GenerateSshKey
+az vm create -n $VM -g $RG --image Ubuntu2204 --generate-ssh-keys --admin-username bestuser --size Standard_D2s_v3 --nsg-rule ssh --public-ip-sku Standard
 
 Start-Sleep -Seconds 2
 # This is the public IP address
 # $vmip=$(az vm list-ip-addresses -g $rg -n $vmName --query "[].virtualMachine.network.publicIpAddresses[0].ipAddress" --output tsv)
-$vmip=$(Get-AzPublicIpAddress -Name $publicIp -ResourceGroupName $rg)
+$vmip=$(az vm list-ip-addresses -g $RG -n $VM --query "[].virtualMachine.network.publicIpAddresses[0].ipAddress" --output tsv)
 
 Start-Sleep -Seconds 1
 Write-Output ""
-Write-Output "The public IP address allocated to VM {$vmName} is {$vmip}"
-Write-Output "The admin user name is: azrez"
+Write-Output "The public IP address allocated to VM {$VM} is {$vmip}"
+Write-Output "Save aside your credentials"
+Write-Output "The admin user name is: {$UserName}"
 Write-Output "The unique password is: {$password}"
 
+Start-Sleep -Seconds 20
 #pwsh
 #$rg='myVM'
 #$location='northeurope'
